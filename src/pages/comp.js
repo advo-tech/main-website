31552ada4281b6604704e9ad36d77f2cff1f4241
Emma Freeman <54911148+emfree19@users.noreply.github.com>
2021-09-12 19:17:02 -0500

    Update comp.js

--- src/pages/comp.js
+++ src/pages/comp.js
@@ -12,16 +12,9 @@ export default () => (
             <h1 class="header">COMP THE HARVARD ADVOCATE</h1>
             <p></p>
             <p class="text">
-   The Harvard Advocate has been a home for arts and literature at Harvard since 1866, and we're looking forward to meeting new members through our virtual Spring 2021 comp! Across its seven boards, The Advocate welcomes critics, artists, writers, designers, programmers, and dealmakers alike. No previous experience is necessary. You don’t have to be a writer or an artist—as long as you’re interested in what we do, Advo has a place for you. 
-   The Advocate will be holding comps for all seven boards: Art, Features, Fiction, Poetry, Design, Business, and Tech. To learn more about the organization, our boards, and the comp process, visit our <a href = "https://www.instagram.com/harvardadvocate/"> Instagram </a>and<a href = "https://docs.google.com/forms/d/e/1FAIpQLSePoDzWsUZV5JYzHLsy5Vuio-oxEKNydTsnmo_wLU4H1EV9ng/viewform"> sign up</a> for one of our intro comp meetings.
-   <ul>
-      <li>In-person at 21 South Street: Tuesday, Sept. 7 @ 9pm ET and Wednesday, Sept. 8 @ 9pm ET</li>
-      <li>On Zoom: Thurs, Sept. 9 @ 6pm at <a href="https://harvard.zoom.us/j/91643085399">this link</a></li>
-   </ul>
-
-   Register and get the link on <a href = "https://docs.google.com/forms/d/e/1FAIpQLSePoDzWsUZV5JYzHLsy5Vuio-oxEKNydTsnmo_wLU4H1EV9ng/viewform">this form</a>.
-   To sign up to comp one or more of our boads, fill out <a href = "https://docs.google.com/forms/d/e/1FAIpQLSfmWGYcrESI6qvQfYDsQ-Pr54edHh9ZhgNx7Wm8yjhv7_D6qg/viewform">this form</a>.
-   For any questions, comments or concerns, contact <a href = "mailto:dionysi@theharvardadvocate.com"> dionysi@theharvardadvocate.com </a>.
+               The Harvard Advocate has been a home for arts and literature at Harvard since 1866, and we're always looking forward to meeting new members through our “comp” process! Across its seven boards, The Advocate welcomes critics, artists, writers, designers, programmers, and dealmakers alike. No previous experience is necessary. You don’t have to be a writer or an artist—as long as you’re interested in what we do, Advo has a place for you. The Advocate holds comps for all seven boards: Art, Features, Fiction, Poetry, Design, Business, and Tech. To learn more about the organization, our boards, and the comp process, visit our <a href = "https://www.instagram.com/harvardadvocate/">Instagram</a>, <a href = "https://www.facebook.com/harvardadvocate">Facebook</a>, or <a href = "https://twitter.com/harvardadvocate?lang=en">Twitter.</a>
+               Currently, our Fall 2021 comp process is underway. Please check back for Spring 2022 comp at a later date. 
+               For any questions, comments or concerns, contact <a href = "mailto:dionysi@theharvardadvocate.com"> dionysi@theharvardadvocate.com </a>.
             </p>
         </div>
    </Layout>
