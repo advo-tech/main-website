814d1f9bef8b991d38961526c59cef92adb7852b
Albert Zhang <albert.b.zhang@gmail.com>
2022-03-14 16:54:22 -0400

    boardhead update

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,25 +10,24 @@ export default () => (
     <div className="container">
         <h1 className="header">ABOUT US</h1>
         <p className="text text-center"> <i>The Harvard Advocate</i>, founded in 1866, is the oldest continuously published collegiate literary magazine in the country. Over its long history, it can count T.S. Eliot, Conrad Aiken, and Norman Mailer among its members and e.e. cummings, Jack Kerouac, and Tom Wolfe as contributors to its pages. A quarterly magazine, <i>The Advocate</i>'s mission is to publish the best art, fiction, poetry, and prose that the Harvard undergraduate community offers. </p>
-        <h2 className="subheader">EXECUTIVE BOARD 2020 </h2>
+        <h2 className="subheader">EXECUTIVE BOARD 2022 </h2>
         <ul className="board-list list-left">
             <li>President</li>
             <li>Publisher</li>
             <li>Art Editors</li>
             <li>&ensp;</li>
             <li>Business Manager</li>
-            <li>Design Editors</li>
-            <li>&ensp;</li>
+            <li>Design Editor</li>
             <li>Features Editor</li>
             <li>Fiction Editor</li>
             <li>Poetry Editor</li>
-            <li>Technology Editors</li>
-            <li>&ensp;</li>
-            <li>Blog Editor</li>
+            <li>Technology Editor</li>
+            <li>Blog Editors</li>
             <li>&ensp;</li>
             <li>Diversity and Inclusivity Chair</li>
             <li>Alumni Relations Manager</li>
             <li>Dionysi</li>
+            <li>&ensp;</li>
             <li>&ensp;</li>
             <li>Pegasi</li>
             <li>&ensp;</li>
@@ -36,38 +35,41 @@ export default () => (
             <li>&ensp;</li>
             <li>Hermes</li>
             <li>&ensp;</li>
+            <li>Merch Director</li>
             <li>Hestia</li>
-            <li>&ensp;</li>
+            <li>Experimental Labs Director</li>
             <li>Librarian</li>
+            <li>&ensp;</li>
         </ul>
         <ul className="board-list list-right">
-            <li>MADI HOWARD</li>
-            <li>MARLENA SKRABAK</li>
-            <li>JOHANNA ALSTOTT</li>
-            <li>JESS LEE</li>
-            <li>YEJIN KIM</li>
             <li>ALBERT ZHANG</li>
-            <li>TOMASZ CIENKOWSKI</li>
-            <li>REBECCA CADENHEAD</li>
-            <li>JULIET ISSELBACHER</li>
-            <li>CHARLEY HUTCHISON</li>
-            <li>EMMA FREEMAN</li>
-            <li>ANDY WANG</li>
-            <li>MARIE UNGAR</li>
-            <li>VICKY XU</li>
-            <li>LANA REEVES</li>
-            <li>LARA ZENG</li>
-            <li>ROSIE COLLIER</li>
-            <li>TADHG LARABEE</li>
-            <li>EZRA LEBOVITZ</li>
-            <li>MICHELLE TONG</li>
-            <li>LILY CUNNINGHAM</li>
-            <li>VICTORIA SANCHEZ</li>
             <li>HARPER ORECK</li>
+            <li>ZOE HOPKINS</li>
+            <li>ROSIE COLLIER</li>
+            <li>GRACE PARKERSON</li>
+            <li>PRIMO LAGASO GOLDBERG</li>
+            <li>PAUL SULLIVAN</li>
             <li>TALIA BLATT</li>
-            <li>ALISA REGASSA</li>
+            <li>EZRA LEBOVITZ</li>
+            <li>NIKHIL DHARMARAJ</li>
+            <li>FRIDO WICKE</li>
+            <li>THEA CHUNG</li>
+            <li>LANA REEVES</li>
+            <li>CHARLOTTE PONTIFELL</li>
+            <li>MAX SCHACHERMAYER</li>
+            <li>JULIET COE</li>
+            <li>ANNIKA INAMPUDI</li>
+            <li>PROOF SCHUBERT-REED</li>
+            <li>MIRA ALPERS</li>
             <li>BETSY PEINADO</li>
-            <li>KEVIN CHEN</li>
+            <li>JESS LEE</li>
+            <li>JULIET ISSELBACHER</li>
+            <li>IZZY CHO</li>
+            <li>NICOLE FARINA</li>
+            <li>ALISA REGASSA</li>
+            <li>LILY CUNNINGHAM</li>
+            <li>ALEXA MARK</li>
+            <li>TADHG LARABEE</li>
         </ul>
     </div>
 
